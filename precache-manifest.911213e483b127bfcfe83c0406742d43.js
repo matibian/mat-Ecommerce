0f8135a40b75824f6657f0febbbdb40fdc0ed8fe
Mati trabajo <matubianchi@gmail.com>
2022-09-22 12:53:20 -0300

    Updates

---
+++ precache-manifest.911213e483b127bfcfe83c0406742d43.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "36ea5d54aaaa24cedeb6",
+    "url": "/static/css/main.b858d7f6.chunk.css"
+  },
+  {
+    "revision": "36ea5d54aaaa24cedeb6",
+    "url": "/static/js/main.36ea5d54.chunk.js"
+  },
+  {
+    "revision": "e0951f3e1edda8b7f479",
+    "url": "/static/js/1.e0951f3e.chunk.js"
+  },
+  {
+    "revision": "dda042118594ad4279b0",
+    "url": "/static/css/2.2431c6c4.chunk.css"
+  },
+  {
+    "revision": "dda042118594ad4279b0",
+    "url": "/static/js/2.dda04211.chunk.js"
+  },
+  {
+    "revision": "6197523b7a637a52b588",
+    "url": "/static/js/runtime~main.6197523b.js"
+  },
+  {
+    "revision": "c81c090879b9a798c64d52cab3c10302",
+    "url": "/static/media/logosoloblanco.c81c0908.svg"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "25e425db8efe1c900689102357f66f7b",
+    "url": "/static/media/logo.25e425db.svg"
+  },
+  {
+    "revision": "03fded62699e6641161a7e28af4c8289",
+    "url": "/static/media/logo.03fded62.png"
+  },
+  {
+    "revision": "9dcc97e1d727b52725e5d3e8acf0dc90",
+    "url": "/index.html"
+  }
+];
